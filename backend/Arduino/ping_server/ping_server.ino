c5780a62c4c5e408ddf27c1fc5e965802ac88f20
Baycken <bei.liu@mail.mcgill.ca>
2014-02-22 22:56:59 -0500

    zzzzzzzzzzzz

---
+++ backend/Arduino/ping_server/ping_server.ino
@@ -0,0 +1,91 @@
+/**
+ * An Mirf example which copies back the data it recives.
+ *
+ * Pins:
+ * Hardware SPI:
+ * MISO -> 12
+ * MOSI -> 11
+ * SCK -> 13
+ *
+ * Configurable:
+ * CE -> 8
+ * CSN -> 7
+ *
+ */
+
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
+
+void setup(){
+  Serial.begin(28800);
+  
+  /*
+   * Set the SPI Driver.
+   */
+  Mirf.cePin = 9;
+  Mirf.csnPin = 10;
+  Mirf.spi = &MirfHardwareSpi;
+  
+  /*
+   * Setup pins / SPI.
+   */
+   
+  Mirf.init();
+  
+  /*
+   * Configure reciving address.
+   */
+   
+  Mirf.setRADDR((byte *)"serv1");
+  
+  /*
+   * Set the payload length to sizeof(unsigned long) the
+   * return type of millis().
+   *
+   * NB: payload on client and server must be the same.
+   */
+   
+  Mirf.payload = sizeof(long);
+  
+  /*
+   * Write channel and payload config then power up reciver.
+   */
+   
+  Mirf.config();
+  
+  Serial.println("Listening..."); 
+}
+
+void loop(){
+  /*
+   * A buffer to store the data.
+   */
+   
+  byte data[Mirf.payload];
+  
+  /*
+   * If a packet has been recived.
+   *
+   * isSending also restores listening mode when it 
+   * transitions from true to false.
+   */
+   
+  if(!Mirf.isSending() && Mirf.dataReady()){
+    Serial.println("Got packet");
+    
+    /*
+     * Get load the packet into the buffer.
+     */
+     
+    Mirf.getData(data);
+    /*
+     * Set the send address.
+     */
+     
+     
+    Mirf.setTADDR((byte *)"clie1");  
+    Mirf.send(data);
+  }
+}
